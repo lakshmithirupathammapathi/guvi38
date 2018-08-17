3cb126f5baf45e0ac0d2ecfce251e7f5c1064c4d
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-17 12:31:43 +0530

    Create guvi38.java

---
+++ guvi38.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int a=150,b=160,t;
+	System.out.println("values before swapping");
+	System.out.println("a="+a);
+	System.out.println("b="+b);
+	t=a;
+	a=b;
+	b=t;
+	System.out.println("values after swapping");
+		System.out.println("a="+a);
+	System.out.println("b="+b);
+
+	
+	}
+}
